8ca75d1870e5cf67561805b9ac47c3031ddccc29
Anshu Misra <anshumisra.25@gmail.com>
2025-01-26 22:00:16 +0530

    code for openai

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-import { assert } from 'console';
-import {db} from './db'
+import {db} from './db/index'
 import { todosTable } from './db/schema'
 import { ilike,eq } from 'drizzle-orm';
+import readlineSync from 'readline-sync';
+import OpenAI from "openai";
+
+
+const client = new OpenAI();
 
 async function getAllTodos() {
   const todos=await db.select().from(todosTable)
@@ -9,18 +13,29 @@ async function getAllTodos() {
 }
 
 async function createTodo(todo) {
-    await db.insert(todosTable).values({
+    const [result]=await db.insert(todosTable).values({
         todo,
+    }).returning({
+        id: todosTable.id,
     });
+
+    return result.id;
 }
 
 async function searchTodo(search) {
-    const todos=await db.select().from(todosTable).where(todosTable.todo.ilike(`%${search}%`))
+    const todos=await db.select().from(todosTable).where(ilike(todosTable.todo,`%${search}%`))
     return todos;
 }
 
 async function deleteTodoById(id) {
     await db.delete(todosTable).where(todosTable.id.eq(id));
+}
+
+const tools={
+    getAllTodos:getAllTodos,
+    createTodo:createTodo,
+    searchTodo:searchTodo,
+    deleteTodoById:deleteTodoById,
 }
 
 const SYSTEM_PROMPT=`
@@ -47,5 +62,33 @@ START
 {"type":"user","user":"I want to shop for milk and bread"}
 {"type:"plan","plan":"I will use createTodo method to add the task to the database"}
 {"type":"action","function":"createTodo","input":"Shopping groceries"}
-`
+{"type":"observation","observation":"2"}
+{"type":"output", "output":"Todo added successfully"}
+`;
+
+while(true){
+    const chat=await client.chat.completions.create({
+        model: "gpt-4o",
+        messages: messages,
+        response_format: {type: "json_object"},
+    });
+    const result=chat.choices[0].message.content;
+    message.push({role:'assistant',content:result});
+
+    const action=JSON.parse(result);
+
+    if(action.type==='output'){
+       console.log(`{action.output}`);
+       break;
+    }else if(action.type==='action'){
+        const fn=tools[action.function];
+        if(!fn) throw new Error('Invalid tool call');
+        const observation=await fn(action.input);
+        const observationMessage={
+            type:'observation',
+            observation:observation,
+        }
+        messages.push({role:'developer',content:JSON.stringify(observationMessage)});
+    }
+}
 
